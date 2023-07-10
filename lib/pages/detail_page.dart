ecd4d2823067fd8f38a72087440abde1e2fef91f
tarripewe01 <tarripewe01@gmail.com>
2023-07-10 14:05:03 +0700

    DetailPage with Statefull Widget, Variant Button & Leading Icon

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:test_flutter/class/item_class.dart';
 import 'package:test_flutter/core/constants.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   const DetailPage({
     super.key,
     required this.cardContent,
@@ -11,24 +11,86 @@ class DetailPage extends StatelessWidget {
   final ItemClass cardContent;
 
   @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  double fontSizeCustom = 25.0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         // if want to hide the arrow left or leading
         // automaticallyImplyLeading: false,
-        title: Text(cardContent.title),
+        title: Text(widget.cardContent.title),
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            // how to show snackbar
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Info '),
+                  behavior: SnackBarBehavior.floating,
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.info,
+            ),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(kDouble10),
+          padding: const EdgeInsets.all(kDouble5),
           child: Column(
             children: [
-              Image.asset(cardContent.imagePath),
+              Image.asset(widget.cardContent.imagePath),
+              Wrap(
+                spacing: kDouble10,
+                // show you the type of buttons
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 25;
+                      });
+                    },
+                    child: const Text('Small Title'),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 50;
+                      });
+                    },
+                    child: const Text('Medium Title'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 100;
+                      });
+                    },
+                    child: const Text('Large Title'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      setState(() {
+                        fontSizeCustom = 200;
+                      });
+                    },
+                    child: const Text('Huge Title'),
+                  ),
+                ],
+              ),
               FittedBox(
                 child: Text(
-                  cardContent.title,
-                  style: const TextStyle(
-                    fontSize: 200,
+                  widget.cardContent.title,
+                  style: TextStyle(
+                    fontSize: fontSizeCustom,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
