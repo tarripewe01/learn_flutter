f6d36ab26609e1f234f413e632f41aacca021199
tarripewe01 <tarripewe01@gmail.com>
2023-07-10 14:45:12 +0700

    Profile Page

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,5 @@
 const double kDouble5 = 5.0;
 const double kDouble10 = 10.0;
+const double kDouble20 = 20.0;
 
 const String detailDescription = 'Bacon ipsum dolor amet boudin turducken fatback tenderloin buffalo beef ribs drumstick corned beef. Filet mignon burgdoggen chislic shoulder sausage, alcatra rump t-bone pig frankfurter prosciutto swine. Corned beef spare ribs pork, andouille ribeye swine venison. Drumstick pork belly jerky, hamburger fatback filet mignon t-bone beef chislic. Pancetta cow beef ribs andouille filet mignon brisket jerky turkey doner salami meatball pork pork loin kevin cupim. Turkey pork belly short ribs pork loin turducken drumstick. Sirloin chicken tri-tip meatloaf picanha boudin tenderloin.';
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:test_flutter/core/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        backgroundColor: Colors.green,
+      ),
+      body: const Column(
+        children: [
+          SizedBox(height: kDouble20),
+          CircleAvatar(
+            backgroundImage: AssetImage('images/yeah.png'),
+            radius: 70,
+          ),
+          SizedBox(height: kDouble20),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Learning flutter'),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('info@samsantech.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text('SamsanTech'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.dark_mode),
+      ),
+    );
   }
 }
