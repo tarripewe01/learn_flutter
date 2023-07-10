dcaac4df99221d191d4a82b430d5bb0caf20a69a
tarripewe01 <tarripewe01@gmail.com>
2023-07-10 11:59:33 +0700

    DetailPage

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:test_flutter/class/item_class.dart';
+import 'package:test_flutter/core/constants.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+  const DetailPage({
+    super.key,
+    required this.cardContent,
+  });
+
+  final ItemClass cardContent;
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +16,41 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(
         // if want to hide the arrow left or leading
         // automaticallyImplyLeading: false,
+        title: Text(cardContent.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(kDouble10),
+          child: Column(
+            children: [
+              Image.asset(cardContent.imagePath),
+              FittedBox(
+                child: Text(
+                  cardContent.title,
+                  style: const TextStyle(
+                    fontSize: 200,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Text(
+                detailDescription,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: kDouble10),
+              const Text(
+                detailDescription,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/widget/card_widget.dart
+++ lib/widget/card_widget.dart
@@ -19,7 +19,9 @@ class CardWidget extends StatelessWidget {
           context,
           MaterialPageRoute(
             builder: (context) {
-              return const DetailPage();
+              return DetailPage(
+                cardContent: cardContent,
+              );
             },
           ),
         );
